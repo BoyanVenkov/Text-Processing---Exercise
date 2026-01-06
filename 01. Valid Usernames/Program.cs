b07809219f31cc07bfc6c37d973b141c670e7c8f
Boyan Venkov <boyanvenkov@yahoo.com>
2026-01-06 18:46:07 +0200

    Add project files.

---
+++ 01. Valid Usernames/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] usernames = Console.ReadLine().Split(", ");
+
+foreach (string username in usernames)
+{
+    if (IsValid(username))
+    {
+        Console.WriteLine(username);
+    }
+}
+
+
+static bool IsValid(string username)
+{
+    if (username.Length < 3 || username.Length > 16)
+    {
+        return false;
+    }
+
+    foreach (char ch in username)
+    {
+        if (!(char.IsLetterOrDigit(ch) || ch == '-' || ch == '_'))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
