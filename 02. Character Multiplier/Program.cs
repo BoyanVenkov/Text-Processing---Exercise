1b10b9aa4de221c5412a9de2bf68c357eed8084f
Boyan Venkov <boyanvenkov@yahoo.com>
2026-01-07 18:14:32 +0200

    Added a New Project

---
+++ 02. Character Multiplier/Program.cs
@@ -0,0 +1,35 @@
+﻿ string[] input = Console.ReadLine().Split(' ');
+
+    string str1 = input[0];
+    string str2 = input[1];
+
+    int result = MultiplyCharacters(str1, str2);
+    Console.WriteLine(result);
+
+static int MultiplyCharacters(string str1, string str2)
+{
+    int sum = 0;
+    int minLength = Math.Min(str1.Length, str2.Length);
+
+    for (int i = 0; i < minLength; i++)
+    {
+        sum += str1[i] * str2[i];
+    }
+
+    if (str1.Length > str2.Length)
+    {
+        for (int i = minLength; i < str1.Length; i++)
+        {
+            sum += str1[i];
+        }
+    }
+    else if (str2.Length > str1.Length)
+    {
+        for (int i = minLength; i < str2.Length; i++)
+        {
+            sum += str2[i];
+        }
+    }
+
+    return sum;
+}
