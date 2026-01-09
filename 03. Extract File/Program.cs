46497fcd2dd96f96174161293661df11be9e4492
Boyan Venkov <boyanvenkov@yahoo.com>
2026-01-09 17:19:18 +0200

    Added a New Project

---
+++ 03. Extract File/Program.cs
@@ -0,0 +1,10 @@
+﻿string path = Console.ReadLine();
+
+string file = path.Substring(path.LastIndexOf('\\') + 1);
+
+int dotIndex = file.LastIndexOf('.');
+string fileName = file.Substring(0, dotIndex);
+string extension = file.Substring(dotIndex + 1);
+
+Console.WriteLine($"File name: {fileName}");
+Console.WriteLine($"File extension: {extension}");
