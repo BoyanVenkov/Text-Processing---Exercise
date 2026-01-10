22cc59296b087afc53e3afac03b98969487bbee4
Boyan Venkov <boyanvenkov@yahoo.com>
2026-01-10 09:34:33 +0200

    Added a New Project

---
+++ 04. Caesar Cipher/Program.cs
@@ -0,0 +1,9 @@
+﻿string text = Console.ReadLine();
+char[] encrypted = new char[text.Length];
+
+for (int i = 0; i < text.Length; i++)
+{
+    encrypted[i] = (char)(text[i] + 3);
+}
+
+Console.WriteLine(new string(encrypted));
