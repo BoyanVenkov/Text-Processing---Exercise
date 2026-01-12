90f4843e5137fccda0f81566f1d908371b209e97
Boyan Venkov <boyanvenkov@yahoo.com>
2026-01-12 19:19:03 +0200

    Added a  New Project

---
+++ 06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("");
+            return;
+        }
+
+        StringBuilder output = new StringBuilder();
+        output.Append(input[0]);
+
+        for (int i = 1; i < input.Length; i++)
+        {
+            if (input[i] != input[i - 1])
+            {
+                output.Append(input[i]);
+            }
+        }
+
+        Console.WriteLine(output.ToString());
+    }
+}
