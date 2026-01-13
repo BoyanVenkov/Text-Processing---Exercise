ef15d00835babb53049fe08133d114216d51bb24
Boyan Venkov <boyanvenkov@yahoo.com>
2026-01-13 18:53:04 +0200

    Added a New Project

---
+++ 07. String Explosion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+string input = Console.ReadLine();
+StringBuilder result = new StringBuilder();
+
+int power = 0;
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i] == '>')
+    {
+        result.Append('>');
+        power += input[i + 1] - '0';
+    }
+    else if (power > 0)
+    {
+        power--;
+    }
+    else
+    {
+        result.Append(input[i]);
+    }
+}
+
+Console.WriteLine(result.ToString());
